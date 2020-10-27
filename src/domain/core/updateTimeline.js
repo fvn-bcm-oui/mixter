044360cf01ce2310d4a5b0ed51e3fb664bbc2c62
Fabien Vignon <fabien.vignon@planete-oui.fr>
2020-10-27 23:05:29 +0100

    Resolve test

--- src/domain/core/updateTimeline.js
+++ src/domain/core/updateTimeline.js
@@ -1,7 +1,18 @@
+var message = require('../../../src/domain/core/message');
+var timelineMessageProjection = require('../../../src/domain/core/timelineMessageProjection');
+
 var UpdateTimeline = function UpdateTimeline(timelineMessageRepository){
     var self = this;
 
     self.register = function(eventPublisher) {
+		eventPublisher.on(message.MessageQuacked, function(message) {
+			const projection = timelineMessageProjection.create(
+				message.author,
+				message.author,
+				message.content,
+				message.messageId);
+			timelineMessageRepository.save(projection)
+		});
     };
 };
 
